aace6fbecd2cb4b1c47fd59543fb0bc346d1a12e
KHANG <Nekosolo2404@gmail.com>
2026-04-14 10:36:11 +0700

    feat: khởi tạo project Next.JS với thông tin cá nhân

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Xin chào 👋</h1>
+      <h2>Đoàn Trương Duy Khang</h2>
+      <p>MSSV: 2111844</p>
+      <p>Lớp: CTK45A</p>
+      <p>Môn: Các công nghệ mới trong PTPM</p>
+    </main>
+  );
+}
